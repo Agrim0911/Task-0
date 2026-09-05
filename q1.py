f12beec85b75c2be7d43d5d68f80bdf772dca738
Agrim0911 <agrim0911@gmail.com>
2026-09-05 16:54:43 +0530

    Add number of even or odd function to q1

--- q1.py
+++ q1.py
@@ -3,10 +3,21 @@ def sum(nums):
     for num in nums:
         total += num
     return(total)
+def even(nums):
+    even = 0
+    odd = 0
+    for num in nums:
+        if num%2 == 0:
+            even+=1
+        else:
+            odd+=1
+    return(even,odd)
 n = int(input("Enter how many numbers you want in the list: "))
 nums = []
 for i in range(n):
     num = int(input("Enter number: "))
     nums.append(num)
 print(nums)
-print("Sum: ",sum(nums))
+print("Even Count: ",even(nums)[0])
+print("Odd Count: ",even(nums)[1])
+# print("Sum: ",sum(nums))
