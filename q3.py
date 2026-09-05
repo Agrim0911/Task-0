ef86321d34972843eeee8f57da8bf472d28d241a
Agrim0911 <agrim0911@gmail.com>
2026-09-05 18:52:20 +0530

    Completed basic functionalities of q3

---
+++ q3.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            prime = False
+            break
+    else:
+        return(prime)
+
+n= int(input("Prime numbers till: "))
+primes=[]
+for i in range(1,n+1):
+    if is_prime(i):
+        primes.append(i)
+print(primes)
