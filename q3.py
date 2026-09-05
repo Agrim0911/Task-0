1eeecf3de0c67fde3714fb45dd404a7f3efa1d44
Agrim0911 <agrim0911@gmail.com>
2026-09-05 18:53:22 +0530

    Added check for number less than or equal to 1

--- q3.py
+++ q3.py
@@ -1,15 +1,22 @@
 def is_prime(num):
-    prime = True
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            prime = False
-            break
+    if num <= 1:
+        return(False)
     else:
-        return(prime)
+        prime = True
+
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                prime = False
+                break
+        else:
+            return(prime)
 
 n= int(input("Prime numbers till: "))
 primes=[]
 for i in range(1,n+1):
     if is_prime(i):
         primes.append(i)
-print(primes)
+if not primes:
+    print("No primes")
+else:   
+    print(primes)
